7d780ddd65d96a9bcec9250d1ba403d5d3fbe70f
dimauloagustin <dimauloagustin@gmail.com>
2023-02-01 14:41:22 -0300

    fix test name

--- Scarecrow.Test/Controllers/RepositoryControllerTest.cs
+++ Scarecrow.Test/Controllers/RepositoryControllerTest.cs
@@ -2,24 +2,26 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using System.Threading.Tasks;
 using Xunit;
 
-public class BasicTests
+namespace Scarecrow.Test {
+    public class ScarecrowTest
     : IClassFixture<WebApplicationFactory<Program>> {
-    private readonly WebApplicationFactory<Program> _factory;
+        private readonly WebApplicationFactory<Program> _factory;
 
-    public BasicTests(WebApplicationFactory<Program> factory) {
-        _factory = factory;
-    }
+        public ScarecrowTest(WebApplicationFactory<Program> factory) {
+            _factory = factory;
+        }
 
-    [Fact]
-    public async Task Should_return_repositories() {
-        // Arrange
-        var client = _factory.CreateClient();
+        [Fact]
+        public async Task Should_return_repositories() {
+            // Arrange
+            var client = _factory.CreateClient();
 
-        // Act
-        var response = await client.GetAsync("/Repository");
+            // Act
+            var response = await client.GetAsync("/Repository");
 
-        // Assert
-        response.EnsureSuccessStatusCode(); // Status Code 200-299
-        Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+        }
     }
 }
