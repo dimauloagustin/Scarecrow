89e38819d4bb63cd391e631c2afb023e1075bfa3
dimauloagustin <dimauloagustin@gmail.com>
2023-02-16 22:16:23 -0300

    added last controller test

--- test/Scarecrow.Test/Controllers/ProfileRepositoriesControllerTest.cs
+++ test/Scarecrow.Test/Controllers/ProfileRepositoriesControllerTest.cs
@@ -35,5 +35,21 @@ namespace Scarecrow.Test {
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
         }
+
+        [Fact]
+        public async Task Should_return_Ok_and_execute_validations_and_return_results_after_task_ends() {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync("/Profiles/Sample/Repositories/ms-logisticoperator-rest/Validation", null);
+            response.EnsureSuccessStatusCode();
+            System.Threading.Thread.Sleep(5000);
+            response = await client.GetAsync("/Profiles/Sample/Repositories/ms-logisticoperator-rest");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Contains("\"isOk\":false", await response.Content.ReadAsStringAsync());
+        }
     }
 }
